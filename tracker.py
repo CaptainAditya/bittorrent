8824248d24a00f788666aa5bcbdc046a3b62a8f3
CaptainAditya <adi0507890220@gmail.com>
2021-10-06 19:19:48 +0400

    COmpletely implemented UDP and HTTP peer request and response

--- tracker.py
+++ tracker.py
@@ -1,6 +1,6 @@
 from torrent import Torrent
 from bcoding import bencode, bdecode
-from udp import udpTracker
+from udp import udpTrackerAnnouncing, udpTrackerConnecting
 import requests
 import struct
 import random
@@ -11,6 +11,7 @@ from urllib.parse import urlparse
 class Tracker:
     def __init__(self, torrent_path):
         self.torrent_obj = Torrent(torrent_path)
+        self.peers = []
     def get_peer_list(self):
         params = {
             'info_hash': self.torrent_obj.info_hash,
@@ -22,36 +23,69 @@ class Tracker:
             'event' : 'started'
         }
         for url in self.torrent_obj.announce_list:
-            if "udp" in url:
+            if "http" in url:
+                # self.http_request(url)
+                pass
+            elif "udp" in url:
                 self.udp_request(url)
+            else:
+                print("Unknown URL format")
+        print(self.peers)
 
     
     def http_request(self, tracker_url):
         params = {
             'info_hash': self.torrent_obj.info_hash,
             'peer_id': self.torrent_obj.peer_id,
-            'uploaded': 0,
-            'downloaded': 0,
-            'port': self.torrent_obj.port,
-            'left': self.torrent_obj.left,
-            'event' : 'started'
+            'left': self.torrent_obj.left,            
         }            
         try:
             answer_tracker = requests.get(tracker_url, params=params, timeout=5)
-            list_peers = bdecode(answer_tracker.content)
-            print(list_peers)
+            print(answer_tracker.status_code)
+            
         except Exception as e:
             print("HTTP scraping failed: %s" % e.__str__())
 
     def udp_request(self, tracker_url):
         url_parse = urlparse(tracker_url)
-        host = ".".join(url_parse.hostname.split(".")[1:])
+        def resolveHost(parse):
+            try:
+                ip, port = socket.gethostbyname(parse.hostname), parse.port
+            except:
+                return "", 0
+            return ip, port
+        ip, port = resolveHost(url_parse)
+        if (ip, port) == ("", 0) or ip == "127.0.0.1":
+            return "DNS"
+        print(ip, port)
+        
+        tracker_connection = udpTrackerConnecting()
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)       
+        sock.settimeout(7) 
+        sock.sendto(tracker_connection.bytestringForConnecting(), (ip, port))
         try:
-            ip, port = socket.gethostbyname(url_parse.hostname), url_parse.port
-        except Exception as e:
-            print(e.__str__())
-        return
+            data, addr = sock.recvfrom(4096)
+        except socket.timeout:
+            return
+        tracker_connection.parse_response(data)
+        sender_port = sock.getsockname()[1]
+
+        tracker_announce = udpTrackerAnnouncing(tracker_connection.server_connection_id, self.torrent_obj.info_hash, 
+                                                self.torrent_obj.peer_id, self.torrent_obj.left, sender_port)
+        sock.sendto(tracker_announce.byteStringAnnounce(), (ip, port))
+        try:
+            data,addr = sock.recvfrom(4096)
+        except:
+            return
+
+        ip_ports = tracker_announce.parse_response(data)
+        
+        for x in ip_ports:
+            self.peers.append(x)
 
 
-t = Tracker("torrent2.torrent")  
+
+    
+t = Tracker("torrent1.torrent")  
 t.get_peer_list()
--- udp.py
+++ udp.py
@@ -1,8 +1,49 @@
 import struct, random
-from socket import socket
-class udpTracker:
-    def __init__(self, udp_tracker):
-        connection_id = struct.pack('>Q', 0x41727101980)
-        action = struct.pack('I', 0)
-        transaction_id = struct.pack('I', random.randint(0,1000))
-        
+from sys import int_info
+class udpTrackerConnecting:
+    def __init__(self):
+        self.connection_id = struct.pack('>Q', 0x41727101980)
+        self.action = struct.pack('>I', 0)
+        trans_id = random.randint(0, 1000)
+        self.transaction_id = struct.pack('>I', trans_id)
+    def bytestringForConnecting(self):
+        return self.connection_id + self.action + self.transaction_id
+    def parse_response(self, data):
+        format_to_unpack = 'IIQ'
+        tracker_connection_response = struct.unpack(format_to_unpack, data)
+        self.server_connection_id = tracker_connection_response[2]
+class udpTrackerAnnouncing:
+    def __init__(self, conn_id, info_hash, peer_id, left, port):
+        self.connection_id = struct.pack('>Q', conn_id)
+        self.action = struct.pack('>I', 1)
+        trans_id = random.randint(0, 1000)
+        self.transaction_id = struct.pack('>I', trans_id)
+        self.info_hash = info_hash
+        self.peer_id = peer_id
+        self.downloaded = struct.pack('>Q', 0)
+        self.left = struct.pack('>Q', left)
+        self.uploaded = struct.pack('>Q', 0)
+        self.event = struct.pack('>I', 0)
+        self.ip = struct.pack('>I', 0)
+        self.key = struct.pack('>I', 0)
+        self.num_want = struct.pack('>i', -1)
+        self.port = struct.pack('>H', port)
+    def byteStringAnnounce(self):
+        return (self.connection_id + self.action + self.transaction_id + self.info_hash + self.peer_id 
+                + self.downloaded + self.left + self.uploaded + self.event + self.ip + self.key + self.num_want + self.port)
+    
+    def parse_response(self, response):
+        server_action = struct.unpack('>I', response[:4])
+        transaction_id = struct.unpack('>I', response[4 : 8])
+        interval = struct.unpack('>I', response[8:12])
+        leechers = struct.unpack('>I' ,response[12:16])
+        seeders = struct.unpack('>I', response[16 : 20])
+        info = list([str(x) for x in response[20:]])
+        ip_port = []
+        i = 0
+        while i < len(info) - len(info) % 6:
+            ip = ".".join(info[i : i + 4])
+            port = int(info[i + 4]) * 256 + int(info[i + 5])
+            ip_port.append((ip, port))
+            i += 6
+        return ip_port
