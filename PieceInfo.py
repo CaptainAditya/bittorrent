9635731feed90b64dcb8393a1c90948a8f652127
Aditya Verma <adi0507890220@gmail.com>
2021-10-27 06:52:42 +0000

    Upload New File

---
+++ PieceInfo.py
@@ -0,0 +1,33 @@
+from torrent import Torrent
+from math import ceil
+from BlockandPiece import Piece
+class PieceInfo:
+    def __init__(self, torrent):
+        self.torrent = torrent
+        self.number_of_pieces = ceil(torrent.total_length / torrent.piece_length)
+        self.pieces = []
+        self.pieces_SHA1 = []
+        self.getSHA1()
+        self.generate_piece()
+
+    def generate_piece(self):
+        last_piece = self.number_of_pieces - 1
+        for i in range(self.number_of_pieces):
+            if i == last_piece:
+                piece_length = self.torrent.total_length - (self.number_of_pieces - 1) * self.torrent.piece_length
+                self.pieces.append(Piece(i, piece_length, self.pieces_SHA1[i]))
+            else:
+                self.pieces.append(Piece(i, self.torrent.piece_length, self.pieces_SHA1[i]))
+    
+    def getSHA1(self):
+        for i in range(self.number_of_pieces):
+            start = i * 20
+            end = start + 20
+            self.pieces_SHA1.append(self.torrent.pieces[start : end])
+
+    def merge_blocks(self, index):
+        res = b""
+        for block in self.pieces[index].blocks:
+            res += block.data
+        return res
+
