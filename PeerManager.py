4d0f99f54d2fa99678b097b63a5b45f55d7cbbf6
adi0507890220 <adi0507890220@gmail.com>
2021-11-04 18:48:44 +0400

    fixed reading for sockets

--- PeerManager.py
+++ PeerManager.py
@@ -17,17 +17,16 @@ class PeerManager:
         self.piece_manager = PieceInfo(self.tracker_obj.torrent_obj)
         self.number_of_pieces = self.piece_manager.number_of_pieces
         self.peers = []
-    
+        self.threads = {}
+
+        self.connected_peers = []
     def connect(self):
         threads = []
         for peer in self.tracker_obj.peers:
             peer_obj = Peer(peer, self.number_of_pieces)
-            t = threading.Thread(target = self.MultiThreadedConnection, args = (peer_obj,))
-            t.start()
-            threads.append(t)
-        for process in threads:
-            process.join()
-        
+            p = threading.Thread(target=self.MultiThreadedConnection, args=(peer_obj, )) 
+            p.start()
+
             # sock = peer_obj.connect_to_peer()
             # if sock:
             #     handshake = Handshake(self.tracker_obj.torrent_obj.peer_id, self.tracker_obj.torrent_obj.info_hash)
@@ -39,64 +38,89 @@ class PeerManager:
             #         threading.Thread(target = self.MultiThreadedDownload, args = (peer_obj, sock, )).start()
             #     except Exception as e:
             #         print(f"HandShake Problem for {peer_obj.ip_port}")
-    def read_continously_from_sock(self, sock, peer):
+    def read_continously_from_sock(self, sock, peer : Peer):
+        print("HELLO")
+        self.connected_peers.append(peer)
         while True:
-            try:
-                length = sock.recv(4)
-                
-                print(length)
-                if len(length) < 4:
-                    break
-                length_u, = struct.unpack(">I", length)
-                if length_u == 1:
-                    message_ID = sock.recv(1)
-                    message_ID_u, = struct.unpack(">B", message_ID)
-                    if message_ID == 0:
-                        print(f"{peer.ip_port} choked")
-                        peer.peer_chocking = 1
-                    elif message_ID == 1:
-                        peer.peer_chocking = 0
-                    elif message_ID == 2:
-                        peer.peer_interested = 1
-                    elif message_ID == 3:
-                        peer.peer_interested = 0
-                elif length_u == 5:
-                    message_ID = sock.recv(1)
-                    message_ID_u, = struct.unpack(">B", message_ID)
-                    if message_ID_u == 4:
-                        piece_i = sock.recv(4)
-                        piece_info = have.parse_response(length + message_ID + piece_i)
-                        peer.bit_field[piece_info.piece_index] = True
-                else:
-                    message_ID = sock.recv(1)
-                    message_ID_u, = struct.unpack(">B", message_ID)
-                    if message_ID_u == 5:
-                        raw_bytes = sock.recv(length_u - 1)
-                        bitField_obj = Bitfield.parse_response(length + message_ID + raw_bytes)
-                        if length_u - 1 == len(raw_bytes):
-                            peer.bit_field = bitField_obj.bitfield
-                        else:
-                            for i, x in enumerate(bitField_obj.bitfield):
-                                peer.bit_field[i] = x
-            except socket.error as e:
-                err = e.args[0]
-                if err != errno.EAGAIN or err != errno.EWOULDBLOCK:
-                    pass
+            length = sock.recv(4)
+            if len(length) < 4:
                 break
-            except Exception:
-                break
+            length_u, = struct.unpack(">I", length)
+            if length_u == 0:
+                continue
+            if length_u == 1:
+                message_ID = sock.recv(1)
+                message_ID_u, = struct.unpack(">B", message_ID)
+                if message_ID == 0:
+                    print(f"{peer.ip_port} choked")
+                    peer.peer_choking = 1
+                elif message_ID == 1:
+                    print(f"{peer.ip_port} unchoked")
+                    peer.peer_choking = 0
+                elif message_ID == 2:
+                    peer.peer_interested = 1
+                elif message_ID == 3:
+                    peer.peer_interested = 0
+            elif length_u == 5:
+                message_ID = sock.recv(1)
+                message_ID_u, = struct.unpack(">B", message_ID)
+                if message_ID_u == 4:
+                    piece_i = sock.recv(4)
+                    piece_info = have.parse_response(length + message_ID + piece_i)
+                    peer.bit_field[piece_info.piece_index] = True
+                    print(f"{peer.ip_port} have message recieved")
+
+            else:
+                message_ID = sock.recv(1)
+                message_ID_u, = struct.unpack(">B", message_ID)
+                if message_ID_u == 5:
+                    raw_bytes = PeerManager._read_piece_data(sock, length_u - 1)
+                    bitField_obj = Bitfield.parse_response(length + message_ID + raw_bytes)
+                    print(f"bitfield for peer with {peer.ip_port} has length {len(peer.bit_field)}")
+                    print(f"bitfield object has {len(bitField_obj.bitfield)}")
+                    if length_u - 1 == len(raw_bytes):
+                        peer.bit_field = bitField_obj.bitfield
+                    else:
+                        for i, x in enumerate(bitField_obj.bitfield[ : len(peer.bit_field)]):
+                            peer.bit_field[i] = x
+                    print(f"{peer.ip_port} bitfield done")
+
+                elif message_ID_u == 7:
+                    index_begin = sock.recv(8)
+                    raw_bytes = PeerManager._read_piece_data(sock, length_u - 9)
+                    piece_obj = pieceMessage.parse_response(length + message_ID + index_begin + raw_bytes)
+                    piece_index, block_length, block_offset, block = piece_obj
+                    block_index = block_offset // BLOCK_SIZE
+                    self.piece_manager.pieces[piece_index].blocks[block_index].data = block
+                    self.piece_manager.pieces[piece_index].blocks[block_index].status = 1        
+                    print(piece_index, block_index, len(raw_bytes)) 
+                    print(f"{peer.ip_port} piece with {piece_index} {block_index} {len(raw_bytes)}")
+               
+            # except socket.error as e:
+            #     err = e.args[0]
+            #     if err != errno.EAGAIN or err != errno.EWOULDBLOCK:
+            #         pass
+            #     print(f"IDHAR PROBLEM HAIN for {peer.ip_port} {e}")
+            #     break
+            # except Exception as f:
+            #     print(f"IDHAR PROBLEM HAIN for {peer.ip_port} {f}")
+            #     break
     @staticmethod
-    def _read_from_socket(sock:socket.socket, piece:Piece, block_index, block_obj : Block):
+    def _read_piece_data(sock, length):
         data = b''
-        sock.settimeout(10)
+        required = 0
+        if length:
+            required = length
         while True:
             try:
-                buff = sock.recv(4096)
-                if len(data + buff) > 16400:
-                    break
+                buff = sock.recv(required)
                 if len(buff) <= 0:
                     break
                 data += buff
+                required = length - len(data)
+                print(required)
+                if len(data) == length:
+                    break
             except socket.error as e:
                 err = e.args[0]
                 if err != errno.EAGAIN or err != errno.EWOULDBLOCK:
@@ -107,12 +131,10 @@ class PeerManager:
                 # logging.exception("Recv failed")
                 break
         if len(data) <= 0:
-            return
-        print(piece.piece_index, block_index, len(data))
-        piece.blocks[block_index].status = 1
-        piece.blocks[block_index].data = data
+            return None
+        return data
         
-    def MultiThreadedConnection(self, peer):
+    def MultiThreadedConnection(self, peer:Peer):
         sock = peer.connect_to_peer()
         if sock != None:
             handshake = Handshake(self.tracker_obj.torrent_obj.peer_id, self.tracker_obj.torrent_obj.info_hash)
@@ -124,25 +146,28 @@ class PeerManager:
                 #check peer_id
                 print("Hand Shake received")
             except Exception as e:
-                print(f"HandShake Problem for {peer.ip_port}")
+                print(f"HandShake Problem for {peer.ip_port} {e}")
                 return
             self.peers.append(peer)
             interested_message = interested()
             sock.send(interested_message.byteStringForInterested())
-            sock.settimeout(7)
-            self.read_continously_from_sock(sock, peer)
-            print(peer.bit_field)
+            peer.am_interested = 1
+            sock.settimeout(None)
+            t = threading.Thread(target=self.read_continously_from_sock, args = (sock, peer, ))
+            self.threads[peer] = t
+            t.start()
         return
 
-    def get_peer_having_piece(self, piece):
+    def get_peer_having_piece(self, piece : Piece):
         index = piece.piece_index
         peers_having_piece = []
-        for peers in self.peers:
+        for peers in self.connected_peers:
             if peers.bit_field[index] == True:
                 peers_having_piece.append(peers)
-        return random.choice(peers_having_piece) if peers_having_piece else None
-            
+        
+        return peers_having_piece
     def request_blockByteString(self, piece, block_index, block):
         request_obj = request(piece.piece_index, block_index * BLOCK_SIZE, block.block_size)
         return request_obj.byteStringForRequest()
 
+    
