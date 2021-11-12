e31b704f941aca991ec70f57cba6be2b8d087dd1
adi0507890220 <adi0507890220@gmail.com>
2021-11-13 00:18:13 +0400

    progress bar and rarest first

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
 from socket import socket
-import struct
-from Messages import Bitfield, keep_alive, pieceMessage
+from Messages import keep_alive
 from peer import Peer
 from torrent import Torrent
 from tracker import Tracker
 from PeerManager import PeerManager
-from BlockandPiece import BLOCK_SIZE, Piece
+from BlockandPiece import Piece
 import socket
 import random
 import threading
 import time
-
+import os, sys
 
 class Bittorrent:
     def __init__(self) -> None:
         pass
-    def start_downloading(self, torrent):
-        throttleSendRateBytes = 0
-        tracker = Tracker(torrent)
+    def startDownloading(self, torrent, path):
+        # throttleSendRateBytes = 0
+        tracker = Tracker(torrent, path)
         tracker.get_peer_list()
         time.sleep(7)
         tracker.exitAllThreads()
+
         p = PeerManager(tracker)
         print(tracker.torrent_obj.total_length)
         p.connect()
         time.sleep(5)
+
         minHeap = p.piece_manager.getRarestPieceMinHeap(p.connected_peers)
-        
         file_thread = threading.Thread(target=p.piece_manager.write_into_file)
         file_thread.start()
 
-        percentage_thread = threading.Thread(target=p.piece_manager.percentage, args = (p.connected_peers, ))
-        percentage_thread.start()
+        # percentage_thread = threading.Thread(target=p.piece_manager.percentage, args = (p.connected_peers, ))
+        # percentage_thread.start()
 
         while p.piece_manager.all_piece_complete() == False:
-            index = None
             for x in minHeap:
                 index = x[0]
                 
@@ -43,20 +42,13 @@ class Bittorrent:
                 peers:Peer = p.get_peer_having_piece(piece)
                 if not peers:                    
                     print("No Peers.. Connecting to peer again...")
-                    time.sleep(4)
+                    time.sleep(10)
                     continue
                 peer:Peer = random.choice(peers)
                 if peer.peer_choking == 1:
                     continue
                 for block_index, block in enumerate(piece.blocks):
-                    # if len(block.data) != 0:
-                    #     continue
-                    # block_index = piece.get_empty_block()
-                    # if piece.blocks[block_index].last_requested:
-                    #     curr = time.time()
-                    #     last = piece.blocks[block_index].last_requested
-                    #     if curr - last < 5:
-                    #         continu    
+                       
                     try:
                         request_block = p.request_blockByteString(piece, block_index, piece.blocks[block_index])
                         if time.time() - peer.last_transmission > 60:
@@ -69,16 +61,20 @@ class Bittorrent:
                         # throttleSendRateBytes += len(request_block)
                         peer.sock.send(request_block)
                         peer.last_transmission = time.time()
-                        # time.sleep(0.05)
+                        
                         # print(f"REQUESTING BLOCK FROM {peer.ip_port} with PIECE INDEX : {piece.piece_index} and BLOCK INDEX : {block_index} from {peer.ip_port}")
                         piece.blocks[block_index].last_requested = time.time()
                     except Exception as e:
                         p.connected_peers.remove(peer)
                         print(f"{e} for {peer.ip_port}")
                         time.sleep(1)
+                p.piece_manager.printProgressBar(p.piece_manager.downloadBlocks(), p.piece_manager.totalBlocks, p.connected_peers, "Progress", f"Rate {p.findRate()}")
+
         print("Torrent Complete")
         p.exitPeerThreads()
         file_thread.join()
         
-d = Bittorrent()
-d.start_downloading("torrents/torrent4.torrent")
+torrent = sys.argv[1]
+path = sys.argv[2]
+b = Bittorrent()
+b.startDownloading(torrent, path)
